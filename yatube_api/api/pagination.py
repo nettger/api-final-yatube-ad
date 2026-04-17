2a68fc1c32018ab26303453a15cf58ac5bdc4f12
nettger <fyyfuthvfy.0305@gmail.com>
2026-04-17 23:05:29 +0300

    Fix posts list pagination behavior

--- yatube_api/api/pagination.py
+++ yatube_api/api/pagination.py
@@ -3,3 +3,8 @@ from rest_framework.pagination import LimitOffsetPagination
 
 class CustomPagination(LimitOffsetPagination):
     default_limit = 10
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if request.query_params.get(self.limit_query_param) is None:
+            return None
+        return super().paginate_queryset(queryset, request, view)
